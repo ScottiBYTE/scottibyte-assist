971170ecf5784e626400c81e32be5edab3cb6b9a
Scott Thompson <vmsman@scottibyte.com>
2026-07-30 22:26:28 -0500

    Add customer voice component test harness

---
+++ src/customer_voice_test.cpp
@@ -0,0 +1,135 @@
+#include "customer_voice_audio.h"
+
+#include <QCoreApplication>
+#include <QTextStream>
+#include <QTimer>
+
+namespace
+{
+
+void printUsage()
+{
+    QTextStream(stderr)
+        << "Usage:\n"
+        << "  customer-voice-test receiver <port> [output-node]\n"
+        << "  customer-voice-test sender <host> <port> [input-node]\n";
+}
+
+}
+
+int main(
+    int argc,
+    char *argv[])
+{
+    QCoreApplication application(
+        argc,
+        argv);
+
+    const QStringList arguments =
+        application.arguments();
+
+    if (arguments.size() < 3) {
+        printUsage();
+        return 1;
+    }
+
+    CustomerVoiceAudio audio;
+
+    QObject::connect(
+        &audio,
+        &CustomerVoiceAudio::statusChanged,
+        &application,
+        [](
+            const QString &message)
+        {
+            QTextStream(stdout)
+                << message
+                << Qt::endl;
+        });
+
+    QObject::connect(
+        &audio,
+        &CustomerVoiceAudio::errorOccurred,
+        &application,
+        [
+            &application
+        ](
+            const QString &message)
+        {
+            QTextStream(stderr)
+                << "Error: "
+                << message
+                << Qt::endl;
+
+            application.exit(2);
+        });
+
+    const QString mode =
+        arguments.at(1).toLower();
+
+    bool started = false;
+
+    if (mode == QStringLiteral("receiver")) {
+        bool validPort = false;
+
+        const quint16 port =
+            arguments.at(2).toUShort(
+                &validPort);
+
+        if (!validPort || port == 0) {
+            printUsage();
+            return 1;
+        }
+
+        const QString outputNode =
+            arguments.size() >= 4
+                ? arguments.at(3)
+                : QString();
+
+        started =
+            audio.startProviderReceiver(
+                port,
+                outputNode);
+    } else if (
+        mode == QStringLiteral("sender")) {
+        if (arguments.size() < 4) {
+            printUsage();
+            return 1;
+        }
+
+        bool validPort = false;
+
+        const quint16 port =
+            arguments.at(3).toUShort(
+                &validPort);
+
+        if (!validPort || port == 0) {
+            printUsage();
+            return 1;
+        }
+
+        const QString inputNode =
+            arguments.size() >= 5
+                ? arguments.at(4)
+                : QString();
+
+        started =
+            audio.startCustomerSender(
+                arguments.at(2),
+                port,
+                inputNode);
+    } else {
+        printUsage();
+        return 1;
+    }
+
+    if (!started) {
+        return 2;
+    }
+
+    QTextStream(stdout)
+        << "Press Ctrl+C to stop."
+        << Qt::endl;
+
+    return application.exec();
+}
