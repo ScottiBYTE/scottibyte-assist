dd158f613e0a0d5dd7d12a117690e05a2c69e114
ScottiBYTE <vmsman@scottibyte.com>
2026-08-02 17:11:04 +0000

    Release Assist server v1.0.0

--- server/src/server.js
+++ server/src/server.js
@@ -151,7 +151,7 @@ app.get(
       status: 'ok',
       service:
         'scottibyte-assist-server',
-      version: '0.9.0',
+      version: '1.0.0',
       protocolVersion: 4,
       websocket: websocketStats(),
       timestamp: new Date().toISOString()
@@ -508,7 +508,7 @@ httpServer.listen(
   host,
   () => {
     console.log(
-      `ScottiBYTE Assist Server v0.9.0 listening on http://${host}:${port}`
+      `ScottiBYTE Assist Server v1.0.0 listening on http://${host}:${port}`
     );
   }
 );
