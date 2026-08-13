2753b91aa8300709cc3aed61be2c1e6d3752bfca
Scott Thompson <vmsman@scottibyte.com>
2026-08-13 00:47:33 -0500

    Add working file transfer offer handshake

--- src/wan_signaling_client.cpp
+++ src/wan_signaling_client.cpp
@@ -1099,8 +1099,13 @@ void WanSignalingClient::createFileTransfer(
             const QJsonObject object =
                 document.object();
 
-            const QString transferId =
+            const QJsonObject transfer =
                 object.value(
+                    QStringLiteral("transfer"))
+                    .toObject();
+
+            const QString transferId =
+                transfer.value(
                     QStringLiteral("id"))
                     .toString()
                     .trimmed();
